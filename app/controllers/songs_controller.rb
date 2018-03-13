c34c56c2b80e431e6cc5c9c38899369c2a414173
Nolan Byrnes <nolanehs@gmail.com>
2018-03-13 17:37:44 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Author.exists? (params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      
+    @song = Song.new(author_id: params[:artist_id])
+  end
   end
 
   def create
